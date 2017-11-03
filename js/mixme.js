cf840678deddb08d615c26926a542215570cd5ed
Henry Wulff <31984992+Mijuuu@users.noreply.github.com>
2017-11-03 13:58:17 +0100

    changed copy and download thingy
    moved copy box into a modal and added a working copy button.
    changed download file name.
    refactoring

--- js/mixme.js
+++ js/mixme.js
@@ -22,25 +22,45 @@ function mix(){
         document.getElementById("class3").innerHTML = shuffledArray[3];
         document.getElementById("class4").innerHTML = shuffledArray[4];
         document.getElementById("class5").innerHTML = shuffledArray[5];
-    
+    }
+    else{
+        $('#notAllNamesEnteredModal').modal();
+    }
+}
+
+function openCopyModal(){
+    if(document.getElementById('class0').textContent !== ""){
+        $('#copyModal').modal();
+        
         var name0 = document.getElementById('name0').value;
         var name1 = document.getElementById('name1').value;
         var name2 = document.getElementById('name2').value;
         var name3 = document.getElementById('name3').value;
         var name4 = document.getElementById('name4').value;
         var name5 = document.getElementById('name5').value;
-    
-        document.getElementById("copyContent").innerHTML =
-           name0 + ' will be playing ' + shuffledArray[0] + "<br/>" + 
-           name1 + ' will be playing ' + shuffledArray[1] + "<br/>" +
-           name2 + ' will be playing ' + shuffledArray[2] + "<br/>" + 
-           name3 + ' will be playing ' + shuffledArray[3] + "<br/>" + 
-           name4 + ' will be playing ' + shuffledArray[4] + "<br/>" + 
-           name5 + ' will be playing ' + shuffledArray[5];
-
-        document.getElementById("timestamp").innerHTML = Date();
+        
+        var shuffledClass0 = document.getElementById("class0").innerHTML;
+        var shuffledClass1 = document.getElementById("class1").innerHTML;
+        var shuffledClass2 = document.getElementById("class2").innerHTML;
+        var shuffledClass3 = document.getElementById("class3").innerHTML;
+        var shuffledClass4 = document.getElementById("class4").innerHTML;
+        var shuffledClass5 = document.getElementById("class5").innerHTML;
+        
+        document.getElementById("copyText").innerHTML = 
+        name0 + ' will be playing ' + shuffledClass0 + "\r" + 
+        name1 + ' will be playing ' + shuffledClass1 + "\r" +
+        name2 + ' will be playing ' + shuffledClass2 + "\r" + 
+        name3 + ' will be playing ' + shuffledClass3 + "\r" + 
+        name4 + ' will be playing ' + shuffledClass4 + "\r" + 
+        name5 + ' will be playing ' + shuffledClass5 + "\r\rBrought to you by mix.tf on mijuuu.github.io";
     }
     else{
-        $('#notAllNamesEnteredModal').modal();
+        $('#tableNotFilledModal').modal();
     }
+}
+
+function copyToClipboard(){
+    var copyText = document.getElementById("copyText");
+    copyText.select();
+    document.execCommand("Copy");
 }
